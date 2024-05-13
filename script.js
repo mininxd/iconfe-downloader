207d2b44dc80d004a7d1430342dba93ce4ecfbb3
Minin <70429604+mininxd@users.noreply.github.com>
2024-05-13 09:35:21 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 import "./hover.js";
-
+import {downloadResource} from "./fileDownload.js";
 
 var repoUrl = "https://api.github.com/repos/andregans/iconfe/contents/SVG";
 
 fetch(repoUrl).then(res => {
   return res.json();
 }).then(data => {
-  
   totalIcons.innerHTML = data.length;
+  
+/*
+  $('.test').on('click', function() {
+    downloadResource(data[0].download_url)})
+*/
+
   // Making Icon List
-  data.forEach((item) => {
+    data.forEach((item) => {
     // create Element
     let  gridItem = document.createElement("div");
     let  gridItemBottom = document.createElement("div");
@@ -27,6 +32,8 @@ fetch(repoUrl).then(res => {
     gridItemBottom.classList.add("grid-item-bottom")
     itemName.classList.add("item-name")
     downloadContainer.classList.add('download-container')
+    svgHref.classList.add(`svg-href`);
+    pngHref.classList.add(`png-href`);
     svgDown.classList.add('button','is-small','is-dark')
     pngDown.classList.add('button','is-small','is-dark')
     
@@ -34,17 +41,26 @@ fetch(repoUrl).then(res => {
     icons.src = item.download_url
     gridItem.appendChild(icons)
     
+    // download click event 
+  svgHref.addEventListener("click", () => {
+    downloadResource(item.download_url)
+  })
+  pngHref.addEventListener("click", () => {
+    downloadResource(item.download_url.replaceAll("SVG","PNG").replaceAll("svg","png"))
+  })
+
     // download button
       // svg button
-    svgHref.setAttribute("href", item.html_url)
+    svgHref.setAttribute("download", item.name)
     svgDown.innerText = `SVG`
     svgHref.append(svgDown)
+    
      // png button
     var pngURL = item.download_url.replaceAll("SVG", "PNG").replaceAll("svg","png");
-    pngDown.innerText = `PNG`;
-    pngHref.setAttribute("href", pngURL);
-    pngHref.download = item.name;
+    pngDown.innerText = `PNG`; 
+    pngHref.setAttribute("download", item.name.replace("svg","png"));
     pngHref.append(pngDown)
+    
     // append on download Container
     downloadContainer.append(svgHref);
     downloadContainer.append(pngHref);
@@ -57,6 +73,7 @@ fetch(repoUrl).then(res => {
     itemContainer.appendChild(gridItem);
   })
   
-
-}).catch(e => {console.log(e)})
-
+  
+}).catch(e => {
+  console.error(e);
+})
