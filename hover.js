bf38aab217c6b038937968d8f04ad541fe8f62b3
Minin <70429604+mininxd@users.noreply.github.com>
2024-05-13 13:06:40 +0700

    Add files via upload

--- hover.js
+++ hover.js
@@ -5,8 +5,8 @@ $(document).on('mouseenter', '.grid-item', function(event) {
   var gridBottom = clickedItem.find('.grid-item-bottom').toArray();
   var itemName = clickedItem.find('.item-name').toArray();
   var downloadContainer = clickedItem.find('.download-container').toArray();
-  icon[0].style.height = "50px";
-  icon[0].style.width = "50px";
+  icon[0].style.height = "40px";
+  icon[0].style.width = "40px";
   gridBottom[0].style.height = "50px";
   itemName[0].style.display = "none";
   downloadContainer[0].style.display = "flex";
@@ -20,9 +20,10 @@ $(document).on('mouseleave', '.grid-item', function(event) {
   var gridBottom = clickedItem.find('.grid-item-bottom').toArray();
   var itemName = clickedItem.find('.item-name').toArray();
   var downloadContainer = clickedItem.find('.download-container').toArray();
-  icon[0].style.height = "75px";
-  icon[0].style.width = "75px";
+  icon[0].style.height = "50px";
+  icon[0].style.width = "50px";
   gridBottom[0].style.height = "auto";
   itemName[0].style.display = "block";
   downloadContainer[0].style.display = "none";
 });
+
